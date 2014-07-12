521d124754bdbb5fa6cfe7cedb5d937b29f50f63
renmengye <renmengye@gmail.com>
2014-07-12 09:10:44 -0700

    itinerary

--- js/itinerary.js
+++ js/itinerary.js
@@ -1,13 +1,20 @@
 
 $( document ).ready(function(){
 	var partyid = getURLParameter('partyid');
-	var driverid = getURLParameter('driverid');
+	var userid = getURLParameter('userid');
+	var driverid = "";
 	var passengers = [];
 	var driver = [];
 	getItineraryData(partyid, function(passengerList){
 		for(var i = 0; i < passengerList.length; i++) {
 			var pass = passengerList[i];
-			if(pass.driverid == driverid) {
+			if(pass.id == userid) {
+				driverid = pass.driverid;
+			}
+		}
+		for(var i = 0; i < passengerList.length; i++) {
+			var pass = passengerList[i];
+			if(pass.driverid == userid) {
 				if(pass.spot > 0) {
 					driver = pass;
 					passengers.push(pass);
@@ -17,6 +24,10 @@ $( document ).ready(function(){
 			}
 		}
 		passengers.sort(function(a, b) {return a.order - b.order});
+		for(var i = 0; i < drivers.length; i++) {
+			if(driverid == driverid)
+			$('.itinerary').append(renderPassenger(passengers[i]));
+		}
 		for(var i = 0; i < passengers.length; i++) {
 			$('.itinerary').append(renderPassenger(passengers[i]));
 		}
@@ -27,14 +38,14 @@ $( document ).ready(function(){
 				'<div class="numberCircle">passengerAbbr</div>'+
 				'<b>Driver:</b> passengerName <br>'+
 				'<b>Number:</b> passengerPhone <br>'+
-				'<b>Address:</b><a href="http://maps.google.com/?q=passengerAddressQuery">1 Infinite Loop,<br> passengerAddress</a> <br>'+
+				'<b>Address:</b><a href="http://maps.google.com/?q=passengerMapQuery">1 Infinite Loop,<br> passengerAddress</a> <br>'+
 			  '</div>'+
 			  '<hr>';
 		var html = passengerTemplate.replace('passengerAbbr', passenger.name[0].toUpperCase());
 		html = html.replace('passengerAddress', passenger.address);
 		html = html.replace('passengerName', passenger.name);
 		html = html.replace('passengerPhone', passenger.phone);
-		html = html.replace('passengerAddressQuery', passenger.address.replace(' ', '+'));
+		html = html.replace('passengerMapQuery', passenger.address.replace(' ', '+'));
 		return html;
 	}
 });
