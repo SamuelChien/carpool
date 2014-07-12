9ff0c3d1dde341eb468e2c6c7ea297c07552bb7c
Kevin Zhang <kevinz@slideshare.com>
2014-07-12 03:05:29 -0700

    Fixing test

--- js/test.js
+++ js/test.js
@@ -4,6 +4,9 @@
       "https://licarpool.azure-mobile.net/",
       "bbIcqJfgUPjcZiVNrFujvChujcZgyb85"
       );
+
+    client.getTable("party");
+    
     insertParty("partyName", "Party Address 123 Post Street").done(function (item) {
       insertUser("Kevin", 1234567, item.partyid, "K address", 3);
       insertUser("Mengye", 1234567, item.partyid, "M address", 0);
--- js/util.js
+++ js/util.js
@@ -5,10 +5,10 @@ function getURLParameter(name) {
 
 function insertParty(partyName, address) {
   var item = { partyname : partyName, address : address };
-  TABLE_CLIENT.getTable("party").insert(item);
+  return TABLE_CLIENT.getTable("party").insert(item);
 }
 
 function insertUser(name, phone, partyid, address, spot) {
   var item = {name : name, phone : phone, partyid : partyid, address : address, spot : spot}
-  TABLE_CLIENT.getTable("user").insert(item);
+  return TABLE_CLIENT.getTable("user").insert(item);
 }
