1aeeea94af1413dbc3f9cce62b15417181431f44
Kevin Zhang <kevinz@slideshare.com>
2014-07-12 01:45:45 -0700

    Add util.js and basic of listview

---
+++ js/util.js
@@ -0,0 +1,5 @@
+<script>
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+}
+</script>
