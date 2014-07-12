43f7881385a7dc92c8366b9d928f5ab113a54c4e
Samuel Chien <samuelchien821@gmail.com>
2014-07-12 08:41:14 -0700

    partyid for itiniery

--- js/table.js
+++ js/table.js
@@ -9,7 +9,7 @@ $( document ).ready(function(){
        {
            
            $("#scrollView").append(
-               "<div class='iternaryPerson'><div class='smallNumberCircle'>" + party.passengers[i].name[0] + "</div><b>" + party.passengers[i].name + "<img class='rightBtn' href='/itinerary.html?partyid=" + party.passengers[i].id +"&name=" + party.passengers[i].name  +"' src='css/img/right.jpg' height='42' width='42'><br></div><hr>"
+               "<div class='iternaryPerson'><div class='smallNumberCircle'>" + party.passengers[i].name[0] + "</div><b>" + party.passengers[i].name + "<img class='rightBtn' href='/itinerary.html?partyid=" + party.passengers[i].partyid +"&name=" + party.passengers[i].name  +"' src='css/img/right.jpg' height='42' width='42'><br></div><hr>"
 	  		    );
        }
    });
