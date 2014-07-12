12dacd38432954e6eb5e01525cc5126bdf403ca1
Kevin Zhang <kevinz@slideshare.com>
2014-07-12 04:03:36 -0700

    enable users

--- js/test.js
+++ js/test.js
@@ -8,10 +8,10 @@
     client.getTable("party");
     
     insertParty("partyName", "Party Address 123 Post Street").done(function (item) {
-      // insertUser("Kevin", 1234567, item.partyid, "K address", 3);
-      // insertUser("Mengye", 1234567, item.partyid, "M address", 0);
-      // insertUser("Sam", 1234567, item.partyid, "S address", 0);
-      // insertUser("Josh", 1234567, item.partyid, "J address", 3);
+      insertUser("Kevin", 1234567, item.partyid, "K address", 3);
+      insertUser("Mengye", 1234567, item.partyid, "M address", 0);
+      insertUser("Sam", 1234567, item.partyid, "S address", 0);
+      insertUser("Josh", 1234567, item.partyid, "J address", 3);
     });
   }
   
