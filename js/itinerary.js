98ff95f9db71fe6189f2c891a8314599fb38af5b
renmengye <renmengye@gmail.com>
2014-07-12 07:38:48 -0700

    added itinerary

---
+++ js/itinerary.js
@@ -0,0 +1,41 @@
+
+$( document ).ready(function(){
+	var partyid = getURLParameter('partyid');
+	var driverid = getURLParameter('driverid');
+	var passengers;
+	var driver;
+	getItineraryData(function(partyid, passengerList){
+		for(var i = 0; i < passengerList.length; i++) {
+			var pass = passengerList[i];
+			if(pass.driverid == driverid) {
+				if(pass.spot > 0) {
+					driver = pass;
+					passengers.push(pass);
+				} else {
+					passengers.push(pass);
+				}
+			}
+		}
+		passenger.sort(function(a, b) {return a.order - b.order});
+		for(var i = 0; i < passengers.length; i++) {
+			$('.itinerary')[0].append(renderPassenger(passengers[i]));
+		}
+	});
+
+	function renderPassenger(passenger) {
+		var passengerTemplate = '
+			  <div class="iternaryPerson">
+				<div class="numberCircle">passengerAbbr</div>
+				<b>Driver:</b> passengerName <br>
+				<b>Number:</b> passengerPhone <br>
+				<b>Address:</b><a href="http://maps.google.com/?q=passengerAddressQuery">1 Infinite Loop,<br> passengerAddress</a> <br>
+			  </div>
+			  <hr>';
+		var html = passengerTemplate.replace('passengerAbbr', passenger.name[0].toUpper());
+		html = html.replace('passengerAddress', passenger.address);
+		html = html.replace('passengerName', passenger.name);
+		html = html.replace('passengerPhone', passenger.phone);
+		html = html.replace('passengerAddressQuery', passenger.address.replace(' ', '+'));
+		return html;
+	}
+}
