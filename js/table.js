2f9a44658a2bc6fd3c52f938f8743f41b97b22c8
kngroo <joshkang@gmail.com>
2014-07-12 12:31:26 -0400

    random color

--- js/table.js
+++ js/table.js
@@ -3,6 +3,9 @@ $( document ).ready(function(){
    var title;
    var list;
    var partyid;
+   var colors = ['#ff0000', '#00ff00', '#0000ff'];
+   var random_color = colors[Math.floor(Math.random()*colors.length)];
+   $('#numberCircle').css('color', random_color);
    list = getParty(partyid, function(party){
        $("#partyTitle").text(party.name);
        for(i=0; i<party.passengers.length; i++)
