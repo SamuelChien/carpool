7347f5c99add1943680466322abca84c0406bfb0
Samuel Chien <samuelchien821@gmail.com>
2014-07-12 10:35:06 -0700

    scope fix

--- js/table.js
+++ js/table.js
@@ -3,19 +3,15 @@ $( document ).ready(function(){
    var title;
    var list;
    var partyid;
-/*
-   var colors = ['#ff0000', '#00ff00', '#0000ff'];
-   var random_color = colors[Math.floor(Math.random()*colors.length)];
-   $('#style.numberCircle').css('color', random_color);
-   */
-   var randomIndex = Math.floor((Math.random() * 5) + 1);
-   var colorClass = "colorClass" + randomIndex.toString();
+   
    list = getParty(partyid, function(party){
        $("#partyTitle").text(party.name);
        for(i=0; i<party.passengers.length; i++)
        {
            var partyid = party.passengers[i].partyid;
  		   var userid = party.passengers[i].id;
+ 		   var randomIndex = Math.floor((Math.random() * 5) + 1);
+           var colorClass = "colorClass" + randomIndex.toString();
            $("#scrollView").append(
                "<div class='iternaryPerson'><input type='hidden' id='hiddenValue' name='" + partyid + "' value='" + userid + "'><div class='smallNumberCircle colorClass'>" + party.passengers[i].name[0].toUpperCase() + "</div><b>" + party.passengers[i].name + "<br></div><hr>"
 	  		    );
